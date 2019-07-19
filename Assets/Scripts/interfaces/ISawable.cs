976c5a1601d504aef93df0467414e8a2d11137b8
Farry1 <mail@lukasfabry.de>
2019-07-19 16:28:30 +0200

    again

--- Assets/Scripts/interfaces/ISawable.cs
+++ Assets/Scripts/interfaces/ISawable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawableObject : MonoBehaviour
+public class ISawable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
