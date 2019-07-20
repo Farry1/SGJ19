ad7eaa7586fef6ebb11e7b55688752e1b40847f8
ThaDisclaimer <evgeni.puzankov@gmail.com>
2019-07-20 23:35:32 +0200

    Explosion

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.rigidbody.AddForce(-collision.contacts[0].normal*force, ForceMode2D.Impulse);
+
+    }
+}
--- Assets/Scripts/TorpedoHit.cs
+++ Assets/Scripts/TorpedoHit.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class TorpedoHit : MonoBehaviour
 {
-    
+
+    public GameObject explosionforce;
+    public GameObject explosion;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Saw")
@@ -12,6 +15,8 @@ public class TorpedoHit : MonoBehaviour
             Debug.Log("Deactivate Torpedo");
             Destroy(transform.root.gameObject);
             MasterControl.Instance.Screenshake(3f);
+            GameObject Boom = Instantiate(explosion);
+            Boom.transform.position = this.transform.position;
         }
 
         if(collision.tag == "SawHandle")
@@ -19,6 +24,8 @@ public class TorpedoHit : MonoBehaviour
             Debug.Log("Kaboom!");
             Destroy(transform.root.gameObject);
             MasterControl.Instance.Screenshake(3f);
+            GameObject Boom = Instantiate(explosionforce);
+            Boom.transform.position = this.transform.position;
         }
     }
 }
