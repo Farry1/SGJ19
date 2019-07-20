6c09708fe9296739cd9310f099358310e4cd4e9f
Farry1 <mail@lukasfabry.de>
2019-07-20 22:50:41 +0200

    Torpedo And Submarine Stuff

---
+++ Assets/Scripts/TorpedoMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorpedoMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+
+    public float minSpeed;
+    public float maxSpeed;
+    float actualSpeed;
+
+    void Start()
+    {
+        actualSpeed = CalculateSpeed();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = transform.forward;
+
+        rigidbody.AddForce(transform.right * actualSpeed);
+    }
+
+    private float CalculateSpeed()
+    {
+        return -(Random.Range(minSpeed, maxSpeed));
+    }
+}
