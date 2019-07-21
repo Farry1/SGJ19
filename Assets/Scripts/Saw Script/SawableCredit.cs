e55d7de378b8c10eaf586f6f13c54bd440079e1f
ThaDisclaimer <evgeni.puzankov@gmail.com>
2019-07-21 10:25:13 +0200

    Credits and menu

---
+++ Assets/Scripts/Saw Script/SawableCredit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SawableCredit : SawableObject
+{
+
+   
+
+    public override void Explode()
+    {
+        CreditsControl.Instance.sawProgress++;
+    }
+
+
+}
---
+++ Assets/Scripts/Saw Script/SawableMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SawableMenu : SawableObject
+{
+
+    public string NextScene;
+
+    public override void Explode()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+
+
+}
